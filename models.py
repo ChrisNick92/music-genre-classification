9ba426874ec7b014a5c271f8e896830eea7536b5
Christos Nikou <chrisnick92@gmail.com>
2023-06-14 00:39:43 +0300

    First part completed - Optimization & Inference next

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 import torch.nn as nn
+import numpy as np
+import torch
 
 
 class MLP(nn.Module):
@@ -19,5 +21,43 @@ class MLP(nn.Module):
     def forward(self, x):
         for op in self.body:
             x = op(x)
-            
+
         return x
+
+
+class CNN(nn.Module):
+
+    def __init__(self, H, W, pooling=False):
+        super(CNN, self).__init__()
+        self.H = H
+        self.W = W
+        self.pooling = pooling
+
+        self._build_convolutional_layers()
+        self._build_classifier()
+
+    def _build_convolutional_layers(self):
+        self.channels = [1, 16, 32, 64, 128]
+        convs = []
+        for i, in_channels in enumerate(self.channels[:-1]):
+            convs.append(nn.Conv2d(in_channels=in_channels, out_channels=self.channels[i + 1], kernel_size=5))
+            self.H = int(np.floor(self.H - 4))
+            self.W = int(np.floor(self.W - 4))
+        if self.pooling:
+            convs.append(nn.MaxPool2d(kernel_size=2, padding=1))
+            self.H = int(np.floor(self.H / 2 + 1))
+            self.W = int(np.floor(self.W / 2 + 1))
+        self.convolutional_layers = nn.Sequential(*convs)
+
+    def _build_classifier(self):
+        self.layer_dims = [self.H * self.W * self.channels[-1], 1024, 256, 32, 4]
+        linears = []
+        for i, dims in enumerate(self.layer_dims[:-1]):
+            linears.append(nn.Linear(in_features=dims, out_features=self.layer_dims[i + 1]))
+        self.classifier = nn.Sequential(*linears)
+
+    def forward(self, x):
+
+        x = self.convolutional_layers(x)
+
+        return self.classifier(torch.flatten(x, start_dim=1))
--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ from sklearn.metrics import confusion_matrix, f1_score, accuracy_score, Confusio
 import matplotlib.pyplot as plt
 
 CLASS_MAPPING = {'classical': 0, 'hiphop': 1, 'rock_metal_hardrock': 2, 'blues': 3}
-
+INV_CLASS_MAPPING = {0: 'classical', 1: 'hiphop', 2: 'rock_metal_hardrock', 3: 'blues'}
 
 def load_mfccs(mode):
     data_path = os.path.join("data", mode, "mfccs")
@@ -17,6 +17,13 @@ def load_mfccs(mode):
 
     y = np.array([CLASS_MAPPING[label] for label in y], dtype=np.int64)
 
+    return X, y
+
+def load_spectrograms(mode):
+    data_path = os.path.join("data", mode, "melgrams")
+    X, y = np.load(os.path.join(data_path, "X.npy")).astype(np.float32), np.load(os.path.join(data_path, "labels.npy"))
+    
+    y = np.array([CLASS_MAPPING[label] for label in y], dtype=np.int64)
     return X, y
 
 
@@ -29,6 +36,18 @@ class VectorDataset(Dataset):
     def __len__(self):
         return len(self.y)
 
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+    
+class SpectrogramDataset(Dataset):
+    
+    def __init__(self, X, y):
+        self.X = torch.unsqueeze(torch.from_numpy(X), 1)
+        self.y = torch.tensor(y, dtype=torch.int64)
+        
+    def __len__(self):
+        return len(self.y)
+    
     def __getitem__(self, idx):
         return self.X[idx], self.y[idx]
 
@@ -44,34 +63,34 @@ def training_loop(epochs, model, train_dloader, val_dloader, optim, loss_fn, dev
     for epoch in range(1, epochs + 1):
 
         model.train()
-        with tqdm(train_dloader, unit='batch', leave=False, desc='Training set') as tbatch:
-            for X, y in tbatch:
-                # Forward pass
-                X, y = X.to(device), y.to(device)
-                out = model(X)
-                loss = loss_fn(out, y)
-                train_loss += loss.item()
+        p_bar = tqdm(train_dloader, unit='batch', leave=False, desc='Training set')
+        for X, y in p_bar:
+            # Forward pass
+            X, y = X.to(device), y.to(device)
+            out = model(X)
+            loss = loss_fn(out, y)
+            train_loss += loss.item()
 
-                # Backward pass
-                optim.zero_grad()
-                loss.backward()
-                optim.step()
-            train_loss /= len(train_dloader)
-            train_loss_list.append(train_loss)
+            # Backward pass
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+        train_loss /= len(train_dloader)
+        train_loss_list.append(train_loss)
 
         model.eval()
         y_true, y_pred = [], []
         best_f1 = -np.inf
         with torch.no_grad():
-            with tqdm(val_dloader, unit='batch', leave=False, desc='Validation set') as vbatch:
-                for X, y in vbatch:
-                    X, y = X.to(device), y.to(device)
-                    out = model(X)
-                    loss = loss_fn(out, y)
-                    preds = torch.argmax(out.detach().cpu(), dim=1)
-                    y_true += y.detach().cpu().tolist()
-                    y_pred += preds.tolist()
-                    val_loss += loss.item()
+            p_bar = tqdm(val_dloader, unit='batch', leave=False, desc='Validation set')
+            for X, y in p_bar:
+                X, y = X.to(device), y.to(device)
+                out = model(X)
+                loss = loss_fn(out, y)
+                preds = torch.argmax(out.detach().cpu(), dim=1)
+                y_true += y.detach().cpu().tolist()
+                y_pred += preds.tolist()
+                val_loss += loss.item()
             val_loss /= len(val_dloader)
             val_loss_list.append(val_loss)
             
@@ -101,21 +120,80 @@ def training_loop(epochs, model, train_dloader, val_dloader, optim, loss_fn, dev
     plt.show()
 
 
-def validation_loop(model, test_dloader, loss_fn):
+def validation_loop(model, test_dloader, loss_fn, device='cpu'):
     model.eval()
     total_loss = 0.
     y_true, y_pred = [], []
     with torch.no_grad():
         with tqdm(test_dloader) as tbatch:
             for X, y in tbatch:
+                X, y = X.to(device), y.to(device)
                 out = model(X)
                 loss = loss_fn(out, y)
                 total_loss += loss.item()
 
-                batch_preds = torch.argmax(out, dim=1)
+                batch_preds = torch.argmax(out.cpu(), dim=1)
                 y_true += y.tolist()
                 y_pred += batch_preds.tolist()
         total_loss /= len(test_dloader)
 
     return total_loss, f1_score(y_true, y_pred,
                                 average='macro'), accuracy_score(y_true, y_pred), confusion_matrix(y_true, y_pred)
+    
+
+def train_and_validate(model, epochs, train_dloader, val_dloader, test_dloader, optim, loss_fn, device='cpu'):
+    
+    # Training part
+    best_f1 = -np.inf
+
+    for epoch in range(1, epochs + 1):
+        model.train()
+        for X, y in train_dloader:
+            # Forward
+            X, y = X.to(device), y.to(device)
+            out = model(X)
+            loss = loss_fn(out, y)
+            
+            # Backward
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+        
+        model.eval()
+        y_preds, y_true = [], []
+        with torch.no_grad():
+            for X, y in val_dloader:
+                X, y = X.to(device), y.to(device)
+                out = model(X)
+                loss = loss_fn(out, y)
+                
+                y_true += y.tolist()
+                batch_preds = torch.argmax(out.cpu(), dim=1)
+                y_preds += batch_preds.tolist()
+        
+        f1 = f1_score(y_true, y_preds, average='macro')
+        if f1 >= best_f1:
+            best_f1 = f1
+            torch.save(model.state_dict(), "model.pt")
+            
+    # Validation part
+    
+    model.load_state_dict(torch.load("model.pt"))
+    model.eval()
+    y_preds, y_true = [], []
+    total_loss = 0.
+    
+    with torch.no_grad():
+        for X, y in test_dloader:
+            X, y = X.to(device), y.to(device)
+            out = model(X)
+            loss = loss_fn(out, y)
+            total_loss += loss.item()
+            
+            y_true += y.tolist()
+            batch_preds = torch.argmax(out.cpu(), dim=1)
+            y_preds += batch_preds.tolist()
+        total_loss /= len(test_dloader)
+        
+    return total_loss, f1_score(y_true, y_preds,
+                                average='macro'), accuracy_score(y_true, y_preds), confusion_matrix(y_true, y_preds)
